5d4d47db3ebdeb6eadf18bdaf0f9516863483566
Ou42 <25629874+Ou42@users.noreply.github.com>
2022-12-19 17:52:03 -0800

    day 01 - goal documentation

--- days/01/day-01.hs
+++ days/01/day-01.hs
@@ -1,6 +1,18 @@
 import Data.List (sortBy)
 import Data.List.Split (splitOn)
 
+{-
+    https://adventofcode.com/2022/day/1
+
+    Translation:
+
+        Given a list of grouped numbers...
+
+        (A) find the group w/ the greatest sum
+
+        (B) find the top 3 groups, return their sum
+-}
+
 getCalsPerElf :: String -> [[Int]]
 getCalsPerElf f =
   let calPerElf = splitOn "\n\n" f
@@ -9,11 +21,13 @@ getCalsPerElf f =
 
 day01PartA :: [[Int]] -> String
 day01PartA css =
+  -- (A) find the group w/ the greatest sum
   let calSums = map sum css
   in "Day 01 - Part A: The max total calories = " <> show (maximum calSums)
 
 day01PartB :: [[Int]] -> String
 day01PartB css =
+  -- (B) find the top 3 groups, return their sum
   let calSums = map sum css
       top3 = sum $ take 3 $ sortBy (flip compare) calSums
   in "Day 01 - Part B: Sum of top 3 calories = " <> show top3
