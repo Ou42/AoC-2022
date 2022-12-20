a90a5538ec6facacc8f9cea3efeb0977b7f8f4dd
Ou42 <25629874+Ou42@users.noreply.github.com>
2022-12-19 17:32:55 -0800

    day 01 - Part B - success!

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 ## 2022-12-19
 
-Start of my Advent of Code 2022 Journey!
+### Start of my Advent of Code 2022 Journey!
 
 - .gitignore .txt files ==> input files
 - *any* notes should be in .md or here
+
+### Day 01 - Part A
+
 - start of day-01
 - build up tiny functions
 - test along the way
 - first bug: "/n/n" instead of "\n\n"
+
+### Day 01 - Part B
+
+- sortBy in Data.List not Data.Sort?!
+- reverse sort using (flip compare)
+- there are other ways: "Down" ?!
--- days/01/day-01.hs
+++ days/01/day-01.hs
@@ -1,3 +1,4 @@
+import Data.List (sortBy)
 import Data.List.Split (splitOn)
 
 getCalsPerElf :: String -> [[Int]]
@@ -8,11 +9,19 @@ getCalsPerElf f =
 
 day01PartA :: [[Int]] -> String
 day01PartA css =
-  let calSums   = map sum css
+  let calSums = map sum css
   in "Day 01 - Part A: The max total calories = " <> show (maximum calSums)
-  
+
+day01PartB :: [[Int]] -> String
+day01PartB css =
+  let calSums = map sum css
+      top3 = sum $ take 3 $ sortBy (flip compare) calSums
+  in "Day 01 - Part B: Sum of top 3 calories = " <> show top3
+
 main = do
   f <- readFile "input-01.txt"
   let css = getCalsPerElf f
-  putStrLn $ show $ head css
-  print $ day01PartA css
+
+  putStrLn $ day01PartA css
+
+  putStrLn $ day01PartB css
