4d46532fcb7cba4a1f0ba3a64ce3154d4dc1438d
Ou42 <25629874+Ou42@users.noreply.github.com>
2022-12-22 22:54:47 -0800

    day-03 Part B success!

--- days/03/day-03.hs
+++ days/03/day-03.hs
@@ -1,4 +1,5 @@
 import qualified Data.List as L
+import Data.List.Split (chunksOf)
 import qualified Data.Set as S
 
 {-
@@ -11,6 +12,12 @@ import qualified Data.Set as S
             . convert the item to a value
               [a..z] == [1..26]
               [A..Z] == [27..52]
+            . return sum of all values
+
+        (B) . group every 3 rucksacks
+            . find the one duplicated item in each
+              of the 3 rucksacks
+            . convert item to a value as above
             . return sum of all values
 -}
 
@@ -30,11 +37,29 @@ day03PartA sacks =
     fmap sum $ sequence $
     map ((flip L.elemIndex lu) 
           . head 
-          . (S.toList) 
+          . (S.toList)
           . (\(f,s) -> S.intersection f s)) sacks
+
+rsContentsPartB :: String -> [[Items]]
+rsContentsPartB f =
+  chunksOf 3 $ map S.fromList $ lines f
+
+day03PartB :: [[Items]] -> Maybe Int
+day03PartB sacks =
+  let lu = ' ' : ['a'..'z'] <> ['A'..'Z'] -- 'a' is at index 1 ...
+  in
+    fmap sum $ sequence $
+    map ((flip L.elemIndex lu) 
+         . head
+         . (S.toList)
+         . (\[s1, s2, s3] -> S.intersection s1 (S.intersection s2 s3))) sacks
+
 
 main = do
   f <- readFile "input-03.txt"
   let sacks = rsContents f
 
   putStrLn $ "Part A = " <> show (day03PartA sacks)
+
+  let sacksPartB = rsContentsPartB f
+  putStrLn $ "Part B = " <> show (day03PartB sacksPartB)
