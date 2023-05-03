09cdc273ff31c734a9842a6808a9042c3d0657b2
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-05-02 17:18:27 -0700

    Day-09 - Part B . start Pair coding with OFD

---
+++ days/09/day-09-B.hs
@@ -0,0 +1,21 @@
+module Day09B where
+
+{-
+    Part A
+
+        Alg for (moving) H & T
+
+        Function moveHead (H) a Direction & Distance
+
+        moveHead :: Knot -> Knot -> Dir -> (Knot, Knot)
+        moveHead h t dir =
+            -- Remember Head position.
+            let oldH = h
+            Move Head once in Direction.
+            If <something>  then Do Something
+                            else Do Something else
+
+            If <something>  then Do Something
+                            else Do Something else
+
+-}
