7ccf9e39726334bb53b85e18986c809861f2ea84
Ou42 <25629874+Ou42@users.noreply.github.com>
2023-05-02 18:29:55 -0700

    Day-09 - Part B III . leaving records for tuples ...

--- days/09/day-09-B.hs
+++ days/09/day-09-B.hs
@@ -1,22 +1,36 @@
 module Day09B where
 
+import Prelude hiding (Left, Right) 
+
 {-
     Part A
 
         Alg for (moving) H & T
-
-        Function moveHeadTail' (H) a Direction & Distance
-
-        moveHeadTail :: Knot -> Knot -> Dir -> (Knot, Knot)
-        moveHeadTail h t dir =
-            -- Remember Head position.
-            let oldH = h
-                newH = moveHead h dir
-            in
-                If (adjacent newH t) then (newH, t)
-                                     else (newH, oldH)
-
-
-
-
 -}
+
+data Dir = Left | Right | Up | Down
+
+data Knot = Knot { row :: Int, col :: Int }
+
+-- Function moveHeadTail' (H) a Direction & Distance
+
+moveHeadTail :: Knot -> Knot -> Dir -> (Knot, Knot)
+moveHeadTail h t dir =
+    let oldH = h
+        newH = moveHead h dir
+    in
+        if adjacent newH t then (newH, t)
+                           else (newH, oldH)
+
+moveHead :: Knot -> Dir -> Knot
+moveHead h Left  = h { row = row h- 1 }
+moveHead h Right = h { row = row h + 1 }
+moveHead h Up    = h { col = col h + 1 }
+moveHead h Down  = h { col = col h - 1 }
+
+adjacent :: Knot -> Knot -> Bool
+adjacent _ _ = True
+
+
+
+
